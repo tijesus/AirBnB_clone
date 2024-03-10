08cee9ce49ac513fa9bfeb426ece5f727b172a73
tijesus <emmanueltijesu@gmail.com>
2024-03-10 22:08:12 +0100

    fixed filestorage requirement

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 '''file storage engine for serialization and desialization of data'''
+
 from json import dump, load
 from models.base_model import BaseModel
 from models.user import User
@@ -15,6 +16,7 @@ class FileStorage:
     serializes instances to a JSON file and
     deserializes JSON file to instances
     '''
+
 
     __file_path = 'file.json'
     __objects = {}  # dictionary of insantiated objects
