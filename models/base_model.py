7acdc37c365099607b5b40676a7be1f9b4523c58
tijesus <emmanueltijesu@gmail.com>
2024-03-10 23:39:50 +0100

    requirement error fix

--- models/base_model.py
+++ models/base_model.py
@@ -1,49 +1,78 @@
 #!/usr/bin/python3
-"""A BaseModel class that serves as the backbone to the AirBnB project"""
+'''
+Defines all common attributes and methods for
+model classes
+'''
 
-import uuid
+from uuid import uuid4
 from datetime import datetime
 import models
 
 
-class BaseModel():
-    """BaseModel class initilization"""
+class BaseModel:
+
+    """
+    Methods:
+        save()
+        to_dict()
+    """
 
     def __init__(self, *args, **kwargs):
-        """Initialize the instances creations"""
+        """
+        Creates an id for each BaseModel and keeps track of each time
+        a user is created and updated
+
+        Attrs:
+            id: uuid.uuid4 string
+            creates_at: current datetime when an instance is created
+            updated_at: current datetime when an instance is created
+            and it will be updated every time you change your object
+        """
+
+        format = "%Y-%m-%dT%H:%M:%S.%f"
         if kwargs:
             for key, value in kwargs.items():
-                if key == "__class__":
-                    continue
-                setattr(self, key, value)
-            if "created_at" in kwargs:
-                c_a_str = kwargs['created_at']
-                formmatter = "%Y-%m-%dT%H:%M:%S.%f"
-                self.created_at = datetime.strptime(c_a_str, formmatter)
-            if "updated_at" in kwargs:
-                u_a_str = kwargs['updated_at']
-                formmatter = "%Y-%m-%dT%H:%M:%S.%f"
-                self.updated_at = datetime.strptime(u_a_str, formmatter)
+                if key != '__class__':
+                    if key == 'created_at' or key == 'updated_at':
+                        try:
+                            time = datetime.strptime(value, format)
+                            setattr(self, key, time)
+                        except Exception as Exc:
+                            raise Exc(f"{value} is not in right {format}")
+                    else:
+                        setattr(self, key, value)
         else:
-            self.id = str(uuid.uuid4())
+            self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-        models.storage.new(self)
+            models.storage.new(self)
 
     def save(self):
-        """Save object to file"""
-        self.created_at = datetime.now()
+        """
+        Updates the public instance attribute updated_at
+        and serialises it
+        """
+
+        self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
-        """Returns a dictionary after saving the instance as a key"""
-        obj_dict = self.__dict__.copy()
-        obj_dict['__class__'] = self.__class__.__name__
-        obj_dict['created_at'] = self.created_at.isoformat()
-        obj_dict['updated_at'] = self.updated_at.isoformat()
-        return obj_dict
+        """
+        returns a dictionary containing all
+        keys/values of __dict__ of the instance
+        """
+        
+        instance_dict = self.__dict__.copy()
+        instance_dict.update({'__class__': self.__class__.__name__})
+        instance_dict['created_at'] = instance_dict['created_at'].isoformat()
+        instance_dict['updated_at'] = instance_dict['updated_at'].isoformat()
+        return instance_dict
 
     def __str__(self):
-        """Return a string representation of this instance"""
-        class_name = self.__class__.__name__
-        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
+        '''
+        string representatioon of a BaseModel
+        '''
+
+        string__ = f'[{self.__class__.__name__}] ({self.id})' +\
+            f' {self.__dict__}'
+        return string__
